0e555bff5a682756a6e624056fcf570800499605
Stephen Blatt <sblattj@gmail.com>
2018-06-19 23:07:32 -0700

    Layout Added

--- .eslintrc.js
+++ .eslintrc.js
@@ -25,6 +25,8 @@ module.exports = {
   },
   // add your custom rules here
   rules: {
+    //ignore linebreak styles
+    "linebreak-style": 0,
     // don't require .vue extension when importing
     'import/extensions': ['error', 'always', {
       js: 'never',
